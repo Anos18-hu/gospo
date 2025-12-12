ebef6ccd68c04b86190acb609419bba782fcd983
Anos18-hu <mlikmlin1@gmail.com>
2025-12-12 12:05:22 +0100

    Update base URL in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-        base: '/gospo/', 
+        base: '/https://anos18-hu.github.io/gospo/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
